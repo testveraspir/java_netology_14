64081da4af6db7e6a95bb5d7084d03d183e8bcde
Vera Spiridonova <ver-spiridonova@yandex.ru>
2021-04-27 14:30:07 +0300

    Ticket

---
+++ src/main/java/ru/netology/domain/InformationTicket.java
@@ -0,0 +1,23 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Generated
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InformationTicket implements Comparable<InformationTicket>{
+    private int id;
+    private int price;
+    private String departureAirport;
+    private String arrivalAirport;
+    private String timeTravel;
+
+    @Override
+    public int compareTo(InformationTicket o) {
+        return price - o.price;
+    }
+}
